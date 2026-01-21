e3534048ce38b881381926c6322755f6c1adfa3d
blessona12 <131678256+blessona12@users.noreply.github.com>
2026-01-21 12:01:43 +0200

    feat: tic tac toe game with player X

---
+++ src/main/java/com/test/KataTraining/TicTacToe/TicTacToeGame.java
@@ -0,0 +1,12 @@
+package com.test.KataTraining.TicTacToe;
+
+public class TicTacToeGame {
+
+    private Player currentPlayer = Player.X;
+
+    public Player getCurrentPlayer()
+    {
+        return currentPlayer;
+    }
+
+}
---
+++ src/test/java/com/test/KataTraining/TicTacToe/TicTacToeGameTest.java
@@ -0,0 +1,15 @@
+package com.test.KataTraining.TicTacToe;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TicTacToeGameTest {
+
+    @Test
+    void gameStartWithPlayerX()
+    {
+        TicTacToeGame game=new TicTacToeGame();
+        assertEquals(Player.X,game.getCurrentPlayer());
+    }
+}
