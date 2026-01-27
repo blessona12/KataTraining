504a66ac45895701e61318794f522b7144192fd7
blessona12 <131678256+blessona12@users.noreply.github.com>
2026-01-27 12:21:45 +0200

    Feat: Player one and Two Wins
    Signed-off-by: blessona12 <131678256+blessona12@users.noreply.github.com>

--- src/main/java/com/test/KataTraining/RockPaperScissor/RockPaperScissor.java
+++ src/main/java/com/test/KataTraining/RockPaperScissor/RockPaperScissor.java
@@ -8,7 +8,12 @@ public class RockPaperScissor {
         {
             return "Draw!";
         }
-        return "p2 wins";
+        if((p1.equals("Rock") && p2.equals("Scissor")) || (p1.equals("Paper")&& p2.equals("Rock"))
+        || (p1.equals("Scissor")&&p2.equals("Paper")))
+        {
+            return "Player1 Wins!";
+        }
+        return "Player2 Wins!";
     }
 
 }
--- src/test/java/com/test/KataTraining/RockPaperScissor/RockPaperScissorTest.java
+++ src/test/java/com/test/KataTraining/RockPaperScissor/RockPaperScissorTest.java
@@ -14,4 +14,22 @@ public class RockPaperScissorTest {
         assertEquals("Draw!",rpc.play("scissor","scissor"));
         assertEquals("Draw!",rpc.play("paper","paper"));
     }
+
+    @Test
+    void PlayerOneWins()
+    {
+        RockPaperScissor rpc= new RockPaperScissor();
+        assertEquals("Player1 Wins!",rpc.play("Rock","Scissor"));
+        assertEquals("Player1 Wins!",rpc.play("Paper","Rock"));
+        assertEquals("Player1 Wins!",rpc.play("Scissor","Paper"));
+    }
+
+    @Test
+    void PlayerTwoWins()
+    {
+        RockPaperScissor rpc= new RockPaperScissor();
+        assertEquals("Player2 Wins!",rpc.play("Scissor","Rock"));
+        assertEquals("Player2 Wins!",rpc.play("Rock","Paper"));
+        assertEquals("Player2 Wins!",rpc.play("paper","Scissor"));
+    }
 }
